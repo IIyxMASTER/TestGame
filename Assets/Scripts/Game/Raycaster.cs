15f26c3fb4b7033239dbcbfb9e1c03175d6f9fd5
StepanovDI <IIyxMASTER@gmail.com>
2022-03-29 09:57:41 +0700

    Update Raycaster.cs

--- Assets/Scripts/Game/Raycaster.cs
+++ Assets/Scripts/Game/Raycaster.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class Raycaster : MonoBehaviour
 {
-    [MenuItem("MyMenu/Do Something")]
-    static void DoSomething()
-    {
-        var go = GameObject.Find("MFPController");
-        var playerPosition = go.transform.position;
-        var targetPosition = new Vector3(-1,0, 0.7f);
-        var direction = (targetPosition - playerPosition).normalized;
-        var playerLook = go.transform.forward;
-        var dot = Vector3.Dot(direction, playerLook );
-        float cos = dot;
-        float angle = Mathf.Acos(cos) * Mathf.Rad2Deg;
-        Debug.Log(angle);
-    }
+    // [MenuItem("MyMenu/Do Something")]
+    // static void DoSomething()
+    // {
+    //     var go = GameObject.Find("MFPController");
+    //     var playerPosition = go.transform.position;
+    //     var targetPosition = new Vector3(-1,0, 0.7f);
+    //     var direction = (targetPosition - playerPosition).normalized;
+    //     var playerLook = go.transform.forward;
+    //     var dot = Vector3.Dot(direction, playerLook );
+    //     float cos = dot;
+    //     float angle = Mathf.Acos(cos) * Mathf.Rad2Deg;
+    //     Debug.Log(angle);
+    // }
 }
